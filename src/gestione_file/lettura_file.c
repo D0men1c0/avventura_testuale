50fce46fc481ca3197b0e557598b2d4df43774fe
D0men1c0 <82759768+D0men1c0@users.noreply.github.com>
2021-04-21 11:11:06 +0200

    lettura file

--- src/gestione_file/lettura_file.c
+++ src/gestione_file/lettura_file.c
@@ -1,3 +1,16 @@
+/**
+ * La seguente funzione leggere_file_testo serve a gestire l'apertura del file di testo in modalità di lettura, la quale è gestita mediante
+ * fopen che a sua volta richiama un altra funzione leggere_stringa, che serve per riferirsi al nome del file di testo, in quanto è stato
+ * utilizzato un tipo di dato denominato stringa per poter gestire più comodamente sia la lunghezza della stringa e anche gli stessi caratteri
+ * all'interno di essa. Infatti proprio perchè utilizziamo il tipo di dato, è presente anche la funzione scrivere_carattere, la quale avrà il
+ * compito di memorizzare all'interno della stringa tutti i caratteri presi volta per volta dal file di testo mediante fgetc(fp), fino a quando
+ * i caratteri del file non sono terminati(!feof).
+ * Inoltre alla fine del ciclo , viene richiamata ulteriormente la funzione scrivere carattere in modo tale da gestire il carattere di
+ * fine stringa cioè \0. Infine se l'apertura del file dovesse andare a buon fine, quindi non dandoci alcun errore (!NULL), chiuderemo il file.
+ * Perciò l'utilità di questo modulo risiede nella gestione dei file di testo per poter visualizzare le varie parti della storia, viene
+ * usata un tipo di dato stringa e non una printf per stampare tutti i caratteri, in quanto negli altri moduli non ci servirà solo
+ * la stampa a video del file di testo , ma ad esempio per gestire i movimenti del personaggio no avremo bisogno di una semplice stampa.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "../utility/utility.h"
@@ -20,7 +33,7 @@ stringa leggere_file_testo(stringa nome_file, stringa stringa_file)
 			i++;
 		}
 
-		scrivere_carattere(&stringa_file, i-1, '\0');
+		scrivere_carattere(&stringa_file, i-1, '\0');//serve per gestire il carattere di fine stringa cioè \0
 	}
 	else
 	{
--- src/gestione_file/lettura_file.h
+++ src/gestione_file/lettura_file.h
@@ -1,1 +1,9 @@
+/**
+ * Il seguente modulo, presenta al suo interno la funzione leggere_file_testo che serve per poter leggere i vari caratteri presenti in un
+ * file di testo e memorizzarli in un tipo di dato chiamato stringa, infatti la scelta dell'utilizzo del tipo di dato, è dovuta al fatto
+ * che risulta più comodo gestire la lunghezza e i caratteri che la compongono.
+ * Perciò la funzione ha come parametri il nome del file di testo su cui andremo ad operare, e stringa_file che è la stringa in cui
+ * andremo ad aggiornare i suoi elementi, inserendo i caratteri presenti nel file di testo mediante la funzione fgetc.
+ * Perciò questo modulo verrà utilizzato, ad esempio, per la visualizzazione dei vari spezzoni di storia.
+ */
 stringa leggere_file_testo(stringa nome_file, stringa stringa_file);
