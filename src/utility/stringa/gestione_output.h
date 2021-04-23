275d0911ead6b8238789ee8a8f90930eeea27e60
Tonio635 <antoniodoronzo7@gmail.com>
2021-04-23 16:55:57 +0200

    Output lento e pulizia schermo

---
+++ src/utility/stringa/gestione_output.h
@@ -0,0 +1,18 @@
+/**
+ * Il seguente modulo contiene funzioni utili alla gestione dell'output di una stringa in modalità "lenta".
+ * Ciò è reso possibile attraverso una funzione che permette di ottenere un delay sul programma, così da poterlo
+ * sfruttare per visualizzare una qualsiasi stringa più lentamente e dare quindi un effetto migliore anche agli occhi del giocatore.
+ * Nello specifico è presente una la funzione "ritardare_programma" grazie alla quale, dando in input i millisecondi, è possibile
+ * ottenere un delay di quella tempistica. Nella funzione "rallentare_output" invece ci sono istruzioni utili alla stampa
+ * di un messaggio attraverso l'utilizzo del delay ottenuto con la precedente funzione. La funzione "pulire_schermo" infine
+ * ha il solo compito di ripulire il CMD da tutti i messaggi.
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "stringa.h"
+
+void ritardare_programma(int millisecondi);
+void rallentare_output(stringa messaggio, int millisecondi);
+void pulire_schermo();
