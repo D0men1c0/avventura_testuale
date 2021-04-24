97280f65603bedd6cbfe9f7dcc790eef222e95f8
Matteo Balice <matteobalice02@gmail.com>
2021-04-24 09:31:34 +0200

    main

---
+++ src/avventura_testuale.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "gestione_avventura/gestione_avventura.h"
+
+int main(void)
+{
+	gestire_avventura();				//Gestisce le operazioni all'apertura del programma
+
+	system ("pause");
+
+	return 0;
+}
