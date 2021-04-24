16d6e70f0bdb36abf2e11120bc397c602510395d
Tonio635 <antoniodoronzo7@gmail.com>
2021-04-24 03:21:31 +0200

    Correzione commenti (definitiva)
    Stavolta sul serio

--- src/utility/output/gestione_output.h
+++ src/utility/output/gestione_output.h
@@ -4,7 +4,7 @@
  * Il seguente modulo contiene funzioni utili alla gestione dell'output di una stringa in modalità "lenta".
  * Ciò è reso possibile attraverso una funzione che permette di ottenere un delay sul programma, così da poterlo
  * sfruttare per visualizzare una qualsiasi stringa più lentamente e dare quindi un effetto migliore anche agli occhi del giocatore.
- * Nello specifico è presente una la funzione "ritardare_programma" grazie alla quale, dando in input i millisecondi, è possibile
+ * Nello specifico è presente la funzione "ritardare_programma" grazie alla quale, dando in input i millisecondi, è possibile
  * ottenere un delay di quella tempistica. Nella funzione "rallentare_output" invece ci sono istruzioni utili alla stampa
  * di un messaggio attraverso l'utilizzo del delay ottenuto con la precedente funzione. La funzione "pulire_schermo" infine
  * ha il solo compito di ripulire il CMD da tutti i messaggi.
