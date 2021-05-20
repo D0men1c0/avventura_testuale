742d267c954e58a91978319d9c06630f4524e37c
Tonio635 <antoniodoronzo7@gmail.com>
2021-05-20 11:03:46 +0200

    mini update gestione cella

--- src/gestione_avventura/gestione_mappa.c
+++ src/gestione_avventura/gestione_mappa.c
@@ -19,12 +19,19 @@ void gestire_cella()
 {
 	int cella_attuale;
 	stringa nome_file = "";
+	stringa stringa_file = "";
 
 	nome_file = allocare_stringa(nome_file, 0);
 
 	cella_attuale = leggere_valore_matrice(mappa, leggere_y(pos), leggere_x(pos));
 
-
+	if(cella_attuale % STORIA == 0)
+	{
+		sprintf(nome_file, "storia/[%d][%d].txt", leggere_y(pos), leggere_x(pos));
+		stringa_file = leggere_file_testo(nome_file, stringa_file);
+		rallentare_output(stringa_file, MILLISECONDI);
+		cella_attuale /= STORIA;
+	}
 	if(cella_attuale % POWER_UP_VITA == 0)
 	{
 		scrivere_vita(&giocatore, leggere_vita(giocatore) + 1);
@@ -42,6 +49,8 @@ void gestire_cella()
 		scrivere_vita(&giocatore, leggere_vita(giocatore) - 1);
 		cella_attuale /= MALUS;
 	}
+
+	scrivere_valore_matrice(mappa, leggere_y(pos), leggere_x(pos), cella_attuale);
 
 	free(nome_file);
 }
