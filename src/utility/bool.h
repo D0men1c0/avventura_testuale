f8efc02ef670bf53a6875d1c1695ae8859f03e2f
D0men1c0 <82759768+D0men1c0@users.noreply.github.com>
2021-05-30 23:34:28 +0200

    LAVORO DI 10 ORE PER COMMENTARE QUESTI MODULI

--- src/utility/bool.h
+++ src/utility/bool.h
@@ -1,3 +1,7 @@
+/**
+ * Questo modulo serve per importare il tipo di dato booleano e serve per definire il valore logico falso quando l'intero è 0
+ * vero invece quando l'intero è uguale a 1.
+ */
 #ifndef BOOL_H
 #define BOOL_H
 
