a607c55ae8d7ae1fb66fa94528b8ca8a3c629962
D0men1c0 <82759768+D0men1c0@users.noreply.github.com>
2021-05-30 14:29:50 +0200

    commentato modulo stringa

--- src/utility/stringa/stringa.c
+++ src/utility/stringa/stringa.c
@@ -1,16 +1,23 @@
 /**
  * Il seguente modulo ha il compito di racchiudere le vare funzioni per la gestione del tipo di dato stringa. La funzione leggere_stringa
- * serve per rendere più comodo il ritorno del tipo di dato stringa. La funzione scrivere_stringa, che ha in input una stringa e un vettore
- * di caratteri, restituisce , usando la funzione di libreria strcpy, il vettore di caratteri all'interno della stringa, senza dover copiare
- * carattere per carattere, inoltre dato l'uso di una allocazione dinamica mediante la funzione malloc , non si devono effettuare controlli
- * per la lunghezza della prima stringa con il vettore di caratteri che si vuole copiare copiare. La funzione scrivere_carattere, oltre a
- * scrivere il carattere all'interno della stringa nella posizione indicata dall'indice, presenta un SE che permette di allocare dinamicamente
- * spazio alla stringa se quest'ultima presenta l'indice % 100 = 0. La funzione leggere_carattere invece ha il compito di restituire appunto
- * il carattere cercato all'interno della stringa in una determinata posizione. La funzione allocare_stringa ha il compito di allocare spazio
- * alla stringa mediante la malloc, se gli elementi da inserire all'interno della stringa dovessero essere molti, è presente la funzione
- * realloc, la quale se si dovesse sforare la lunghezza viene richiamata affichè si allochi nuovo spazio alla stringa.
- * Infine la funzione concatenare_stringhe, grazie alla funzione di libreria strcat, restituisce i caratteri della prima stringa alla quale
+ * serve per rendere più comodo il ritorno del tipo di dato stringa. La funzione scrivere_stringa, che ha in input due stringhe restituisce,
+ * usando la funzione di libreria strcpy, la seconda stringa all'interno della prima stringa, senza dover copiare carattere per carattere,
+ * La funzione scrivere_carattere, oltre a scrivere il carattere all'interno della stringa nella posizione indicata dall'indice, presenta
+ * un SE che permette di allocare dinamicamente spazio alla stringa se quest'ultima presenta l'indice % 100 = 0.
+ * La funzione leggere_carattere invece ha il compito di restituire appunto il carattere cercato all'interno della stringa in una determinata
+ * posizione.
+ * La funzione allocare_stringa ha il compito di allocare spazio alla stringa mediante la malloc, se gli elementi da inserire all'interno
+ * della stringa dovessero essere molti, è presente la funzione realloc, la quale se si dovesse sforare la lunghezza viene richiamata
+ * affichè si allochi nuovo spazio alla stringa.
+ * La funzione concatenare_stringhe, grazie alla funzione di libreria strcat, restituisce i caratteri della prima stringa alla quale
  * sono stati concatenati i caratteri presenti nel vettore di caratteri.
+ * La funzione leggere_lunghezza serve a restituire la lunghezza della stringa passata in input grazie alla funzione di libreria strlen.
+ * La funzione confrontare_stringhe ha il compito di restituire un esito (true o false) se le due stringhe passate in input sono uguali.
+ * La funzione shiftare_sinistra ha il compito di shiftare tutta la stringa verso destra di n_ripetizioni cioè dell'intero passato in input.
+ * La funzione convertire stringa minuscolo ha il compito di sostituire i caratteri maiuscoli con quelli minuscoli della stringa
+ * qualora dovessero essere presenti.
+ * Infine la funzione convertire_minuscolo fa la stessa cosa di convertire_stringa_minuscolo ma non restituisce una stringa bensì un singolo
+ * carattere convertito.
  */
 
 #include <stdio.h>
@@ -119,47 +126,13 @@ stringa shiftare_sinistra(stringa str, int ripetizioni)
 			j++;
 		}
 
-		str = scrivere_carattere(str, j, '\0');
+		str = scrivere_carattere(str, j, '\0');                                    //serve per inserire il carattere di fine stringa
 
 		i++;
 	}
 
 
 	return str;
-}
-
-
-int convertire_intero(stringa str)
-{
-	int intero;
-	int i;
-	char c;
-
-	intero = 0;
-
-	c = leggere_carattere(str, 0);
-
-	i = 1;
-
-	while(c != '\0' && c >= '0' && c <= '9')
-	{
-		intero = (c - '0') + intero * 10;
-		c = leggere_carattere(str, i);
-
-		i++;
-	}
-
-	return intero;
-}
-
-char convertire_minuscolo(char c)
-{
-	if(c >= 'A' && c <= 'Z')
-	{
-		  c += 'a' - 'A';
-	}
-
-	return c;
 }
 
 stringa convertire_stringa_minuscolo(stringa str)
@@ -179,4 +152,14 @@ stringa convertire_stringa_minuscolo(stringa str)
 	}
 
 	return str;
+}
+
+char convertire_minuscolo(char c)
+{
+	if(c >= 'A' && c <= 'Z')
+	{
+		  c += 'a' - 'A';
+	}
+
+	return c;
 }
--- src/utility/stringa/stringa.h
+++ src/utility/stringa/stringa.h
@@ -1,8 +1,3 @@
-#ifndef STRINGA_H
-#define STRINGA_H
-
-#include "../bool.h"
-
 /**
  * Questo modulo contiene il tipo di dato stringa con le relative funzioni legate ad esso. Il tipo di dato stringa è composto da un vettore
  * di caratteri. Si è deciso di utilizzare il tipo di dato stringa in quanto riteniamo sia più comodo poter gestire il tipo di dato stringa
@@ -11,17 +6,30 @@
  * scrivere_carattere che ha in input la stringa sulla quale bisogna operare, l'indice cioè la posizione in cui andare ad inserire il
  * carattere, e il carattere e restituisce la stringa di input in cui è stata aggiornata aggiungendo il carattere di input.
  * Funzione simile è leggere_carattere la quale riceve in input la stringa e l'indice e restituisce il carattere che si vuole leggere.
- * La funzione scrivere_stringa che riceve in input una stringa e un char *, ha il compito di copiare tutti i caratteri all'interno della
- * stringa senza dover inserire volta per volta ciascun carattere, infatti restituisce come output i caratteri copiati.
+ * La funzione scrivere_stringa che riceve in input due stringhe , ha il compito di copiare tutti i caratteri all'interno della
+ * stringa senza dover inserire volta per volta ciascun carattere, infatti restituisce come output i la stringa con i caretteri copiati.
  * Inoltre è presente la funzione allocare_stringa in quanto si è scelto di operare su un vettore dinamico e dunque la funzione allocare
  * stringa, che riceve in input la stringa e la dimensione, ha il compito di allocare spazio dinamicamente all'interno della stringa.
- * E' anche presente la funzione concatenare_stringhe che riceve come input 1 stringa e una serie di caratteri, come output resituisce
- * la prima stringa concatenata con i caratteri.
+ * E' anche presente la funzione concatenare_stringhe che riceve come input 2 stringhe, come output resituisce la prima stringa
+ * concatenata con la seconda.
+ * Inoltre è presente la funzione shiftare sinistra che riceve in input una stringa e un intero rappresentante le ripetizioni e restituisce
+ * come output la stringa shiftata da sinistra verso destra di tante posizioni in base al numero delle ripetizioni.
+ * La funzione leggere_stringa_tastiera è dichiarata in questo modulo ma viene usata nel modulo gestione_input_output per coerenza,
+ * riceve in input una stringa e restituisce come output la stringa letta da tastiera.
+ * La funzione leggere_lunghezza riceve in input una stringa e restituisce come output un intero che rappresenta la dimensione della
+ * stringa, usando la funzione di libreria strlen.
+ * La funzione confrontare_stringhe riceve in input due stringhe e come output un valore booleano per indicare se le due stringhe
+ * sono uguali.
+ * Come ultima funzione è stata definita convertire_stringa_minuscolo che ha in input una stringa e restituisce come output la stringa
+ * con le lettere dell'alfabeto convertite da maiuscolo a minuscolo. convertire_minuscolo invece restituisce solo il carattere convertito
+ * avendo in input un altro carattere.
  * Per concludere è stata definita la costante DIM_STRINGA per poter reallocare spazio con l'oppurtuna funzione realloc definita in
  * allocare_stringa.
  */
+#ifndef STRINGA_H
+#define STRINGA_H
 
-
+#include "../bool.h"
 
 #define DIM_STRINGA 100
 
@@ -36,10 +44,9 @@ stringa concatenare_stringhe(stringa str, stringa str_2);
 stringa shiftare_sinistra(stringa str, int ripetizioni);
 stringa leggere_stringa_tastiera(stringa str);
 int leggere_lunghezza(stringa str);
-int convertire_intero(stringa str);
-char convertire_minuscolo(char c);
 bool confrontare_stringhe(stringa str1, stringa str2);
 stringa convertire_stringa_minuscolo(stringa str);
+char convertire_minuscolo(char c);
 
 #endif
 
